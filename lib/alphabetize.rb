ebcd1431539bfee42cd310e20c75f7328bd7dad0
Kah Yap <kahyap13@gmail.com>
2018-12-03 21:58:46 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  hash = {}
+  array = []
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.each { |word| hash[word] = alphabet.index(word[0]) }
+  sorted = hash.sort_by { |k, v| v }
+  sorted.each { |k, v| array << k }
+  array
 end
